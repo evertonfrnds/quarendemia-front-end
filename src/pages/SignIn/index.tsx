13b1299980e126ec30aeeba106084903b092659c
rubensojunior <rubensojunior6@gmail.com>
2020-07-05 21:01:08 -0300

    implementing auth hook

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -9,30 +9,46 @@ import Input from '../../components/Input'
 
 import { Container, Content, Background } from './styles'
 import getValidationErrors from '../../utils/getValidationErrors'
+import { useAuth } from '../../hooks/auth'
+
+interface SignInFormData {
+  email: string
+  password: string
+}
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
 
-  const handleSubmit = useCallback(async (data: Object) => {
-    try {
-      formRef.current?.setErrors({})
+  const { signIn } = useAuth()
 
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('E-mail obrigatório')
-          .email('Digite um e-mail válido'),
-        password: Yup.string().required('Senha obrigatória'),
-      })
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({})
 
-      await schema.validate(data, {
-        abortEarly: false,
-      })
-    } catch (err) {
-      const errors = getValidationErrors(err)
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().required('Senha obrigatória'),
+        })
 
-      formRef.current?.setErrors(errors)
-    }
-  }, [])
+        await schema.validate(data, {
+          abortEarly: false,
+        })
+
+        await signIn({
+          email: data.email,
+          password: data.password,
+        })
+      } catch (err) {
+        const errors = getValidationErrors(err)
+
+        formRef.current?.setErrors(errors)
+      }
+    },
+    [signIn],
+  )
 
   return (
     <Container>
