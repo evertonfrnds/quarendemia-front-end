a8869b56cc6d97aede15b866fc995ea5783e501f
rubensojunior <rubensojunior6@gmail.com>
2020-07-10 23:53:43 -0300

    implementing profile change

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,161 @@
+import React, { useCallback, useRef } from 'react'
+import { FiMail, FiUser, FiLock, FiArrowLeft } from 'react-icons/fi'
+import { FormHandles } from '@unform/core'
+import { Form } from '@unform/web'
+import * as Yup from 'yup'
+import { useHistory, Link } from 'react-router-dom'
+import api from '../../services/api'
+import { useToast } from '../../hooks/toast'
+
+import getValidationErrors from '../../utils/getValidationErrors'
+
+import Button from '../../components/Button'
+import Input from '../../components/Input'
+
+import { Container, Content } from './styles'
+import { useAuth } from '../../hooks/auth'
+
+interface ProfileFormData {
+  name: string
+  email: string
+  old_password: string
+  password: string
+  password_confirmation: string
+}
+
+const Profile: React.FC = () => {
+  const formRef = useRef<FormHandles>(null)
+  const { addToast } = useToast()
+  const history = useHistory()
+
+  const { user, updateUser } = useAuth()
+
+  const handleSubmit = useCallback(
+    async (data: ProfileFormData) => {
+      try {
+        formRef.current?.setErrors({})
+
+        const schema = Yup.object().shape({
+          name: Yup.string().min(6, 'Nome deve ter no mínimo 6 dígitos'),
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('E-mail inválido'),
+          old_password: Yup.string(),
+          password: Yup.string().when('old_password', {
+            is: (val) => !!val.length,
+            then: Yup.string().required('Campo obrigatório'),
+            otherwise: Yup.string(),
+          }),
+          password_confirmation: Yup.string()
+            .when('old_password', {
+              is: (val) => !!val.length,
+              then: Yup.string().required('Campo obrigatório'),
+              otherwise: Yup.string(),
+            })
+            .oneOf(
+              [Yup.ref('password'), undefined],
+              'Confirmação de senha incorreta',
+            ),
+        })
+
+        await schema.validate(data, {
+          abortEarly: false,
+        })
+
+        const {
+          name,
+          email,
+          old_password,
+          password,
+          password_confirmation,
+        } = data
+
+        const formData = {
+          name,
+          email,
+          ...(old_password
+            ? { old_password, password, password_confirmation }
+            : {}),
+        }
+
+        const response = await api.put('/managers', formData)
+
+        updateUser(response.data)
+
+        history.push('/dashboard')
+
+        addToast({
+          type: 'success',
+          title: 'Perfil atualizado com sucesso!',
+          description: 'Suas informações foram atualizadas com sucesso!',
+        })
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err)
+
+          formRef.current?.setErrors(errors)
+
+          return
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro durante atualização',
+          description: 'Ocorreu um erro ao atualizar o perfil, tente novamente',
+        })
+      }
+    },
+    [addToast, history, updateUser],
+  )
+
+  return (
+    <Container>
+      <header>
+        <div>
+          <Link to="/dashboard">
+            <FiArrowLeft />
+          </Link>
+        </div>
+      </header>
+
+      <Content>
+        <Form
+          ref={formRef}
+          initialData={{ name: user.name, email: user.email }}
+          onSubmit={handleSubmit}
+        >
+          <h1>Meu perfil</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            containerStyle={{ marginTop: 24 }}
+            name="old_password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha atual"
+          />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Nova senha"
+          />
+
+          <Input
+            name="password_confirmation"
+            icon={FiLock}
+            type="password"
+            placeholder="Confirmar senha"
+          />
+
+          <Button type="submit">Confirmar mudanças</Button>
+        </Form>
+      </Content>
+    </Container>
+  )
+}
+
+export default Profile
