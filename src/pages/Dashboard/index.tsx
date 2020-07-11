36595349b88d1ecc812955a80b26629466986a28
rubensojunior <rubensojunior6@gmail.com>
2020-07-10 23:43:14 -0300

    implementing dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,36 @@
 import React from 'react'
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>
+import { FiPower } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+import { Container, Header, HeaderContent, Profile, Content } from './styles'
+
+import { useAuth } from '../../hooks/auth'
+
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth()
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <Profile>
+            <div>
+              <span>Bem-vindo,</span>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content />
+    </Container>
+  )
+}
 
 export default Dashboard
